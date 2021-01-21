4b2d9138e884a2d37d1a1a55beab5f6d13d4daeb
GiaDieu <giadieuly@gmail.com>
2021-01-21 21:09:48 +0700

    commit - setUp_AcceptanceProps

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import Congrats from './component/congrats/Congrats';
+import GuessedWords from './component/guessedWords/GuessedWords';
+class App extends Component {
+  render() {
+    const guessedWords = [
+      {
+        guessedWord: 'party',
+        letterMatchCount: 5,
+      },
+    ];
+    return (
+      <div className="container">
+        <h1>Jotto</h1>
+        <Congrats success={true} />
+        <GuessedWords guessedWords={guessedWords} />
+      </div>
+    );
+  }
 }
 
 export default App;
