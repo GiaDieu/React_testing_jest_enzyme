7a8ff23b368d1956955509810ca1ade3033269bc
GiaDieu <giadieuly@gmail.com>
2021-01-22 21:09:53 +0700

    commit - integrationTest

---
+++ src/integration.test.js
@@ -0,0 +1,16 @@
+import { storeFactory } from './test/testUtils';
+import { guessWord } from './actions';
+
+describe('guessWord actiont dispatched', () => {
+  describe('no guessed words', () => {
+    test('renders state correctly for unsuccessful guess', () => {});
+
+    test('renders state correctly for successful guess', () => {});
+  });
+
+  describe('some guessed words', () => {
+    test('renders state correctly for unsuccessful guess', () => {});
+
+    test('renders state correctly for successful guess', () => {});
+  });
+});
