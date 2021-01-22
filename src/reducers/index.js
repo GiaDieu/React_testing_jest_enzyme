fa0e7f05fdb33c1e1375b1fab8d81705c2a26cde
GiaDieu <giadieuly@gmail.com>
2021-01-22 21:10:46 +0700

    add - rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import successReducer from './successReducer';
+import guessedWordReducer from './guessedWordsReducer';
 
 const rootReducer = combineReducers({
   success: successReducer,
+  guessedWords: guessedWordReducer,
 });
 
 export default rootReducer;
