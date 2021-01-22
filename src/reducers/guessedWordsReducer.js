b05f3feeaced10f4e460cb446f9049e16c0fa1d8
GiaDieu <giadieuly@gmail.com>
2021-01-22 21:10:37 +0700

    create - guessedWordsReducer

---
+++ src/reducers/guessedWordsReducer.js
@@ -0,0 +1,11 @@
+/**
+ * @function guessedWordReducer
+ * @param {array} state - Array of Guessed Words
+ * @param {object} action - Action to be reduced
+ * @returns {array} - new guessedWords state
+ */
+const guessedWordReducer = (state, action) => {
+  return null;
+};
+
+export default guessedWordReducer;
