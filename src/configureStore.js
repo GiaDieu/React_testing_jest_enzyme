3b660a73b96dd4f225bd4a268f3d7dc24e3eda07
GiaDieu <giadieuly@gmail.com>
2021-01-22 21:09:44 +0700

    add - ReduxDevTools

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,10 +1,10 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
 import thunk from 'redux-thunk';
 
 export const middlewares = [thunk];
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middlewareEnhancers = composeEnhancers(applyMiddleware(...middlewares));
+const createStoreWithMiddleware = middlewareEnhancers(createStore);
 export default createStoreWithMiddleware(rootReducer);
