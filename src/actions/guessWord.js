2da6dc997ba5d0e51983d89b08d00236b5db2036
GiaDieu <giadieuly@gmail.com>
2021-01-23 14:28:51 +0700

    update - guessWordAction

--- src/actions/guessWord.js
+++ src/actions/guessWord.js
@@ -1,5 +1,5 @@
 import { actionTypes } from './types';
-
+import { getLetterMatchCount } from '../helpers';
 /**
  * Returns Redux Thunk function that dispatch GUESS_WORD action and (conditionally CORRECT_GUESS) action
  * @function guessWord
@@ -7,5 +7,16 @@ import { actionTypes } from './types';
  * @returns {function} - Redux Thunk function
  */
 export const guessWord = (guessedWord) => {
-  return (dispatch, getState) => {};
+  return (dispatch, getState) => {
+    const secretWord = getState().secretWord;
+    const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
+    dispatch({
+      type: actionTypes.GUESS_WORD,
+      payload: { guessedWord, letterMatchCount },
+    });
+
+    if (guessedWord === secretWord) {
+      dispatch({ type: actionTypes.CORRECT_GUESS });
+    }
+  };
 };
