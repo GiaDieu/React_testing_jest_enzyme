44267e6ef366be1c47b4b52ad4131da85d83862d
GiaDieu <giadieuly@gmail.com>
2021-01-23 14:29:33 +0700

    update - guessedWordReducer

--- src/reducers/guessedWordsReducer.js
+++ src/reducers/guessedWordsReducer.js
@@ -1,11 +1,19 @@
+import { actionTypes } from '../actions/types';
+
 /**
  * @function guessedWordReducer
  * @param {array} state - Array of Guessed Words
  * @param {object} action - Action to be reduced
  * @returns {array} - new guessedWords state
  */
-const guessedWordReducer = (state, action) => {
-  return null;
+const guessedWordReducer = (state = [], action) => {
+  switch (action.type) {
+    case actionTypes.GUESS_WORD:
+      return [...state, action.payload];
+
+    default:
+      return state;
+  }
 };
 
 export default guessedWordReducer;
