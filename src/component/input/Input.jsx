b566dcafde0ea7dd8a9796dde45e863e2e7aedd3
GiaDieu <giadieuly@gmail.com>
2021-01-23 18:28:33 +0700

    commit - UnconnectedInput

--- src/component/input/Input.jsx
+++ src/component/input/Input.jsx
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { guessWord } from '../../actions/guessWord';
-class Input extends Component {
+export class UnconnectedInput extends Component {
   render() {
-    const contents = this.props.success ? null : (
-      <form className="form-inline">
-        <input
-          type="text"
-          data-test="input-box"
-          className="mb-2 mx-sm-3"
-          placeholder="enter guess..."
-        />
-        <button
-          data-test="submit-button"
-          type="submit"
-          className="btn btn-primary"
-        >
-          Submit
-        </button>
-      </form>
-    );
+    const contents =
+      this.props.success || this.props.gaveUp ? null : (
+        <form className="form-inline">
+          <input
+            type="text"
+            data-test="input-box"
+            className="mb-2 mx-sm-3"
+            placeholder="enter guess..."
+          />
+          <button
+            data-test="submit-button"
+            type="submit"
+            className="btn btn-primary"
+            onClick={() => this.props.guessWord('train')}
+          >
+            Submit
+          </button>
+        </form>
+      );
     return <div data-test="component-input">{contents}</div>;
   }
 }
@@ -28,4 +30,4 @@ const mapStateToProps = ({ success }) => {
   return { success };
 };
 
-export default connect(mapStateToProps, { guessWord })(Input);
+export default connect(mapStateToProps, { guessWord })(UnconnectedInput);
