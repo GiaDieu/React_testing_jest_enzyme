d1b06ca3fad4071f16274a2e4494b5244745cfcf
GiaDieu <giadieuly@gmail.com>
2021-01-23 14:29:23 +0700

    update - helpers

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -8,7 +8,6 @@ export function getLetterMatchCount(guessedWord, secretWord) {
   const guessedWordSet = new Set(guessedWord.split(''));
   const secretWordSet = new Set(secretWord.split(''));
 
-  return Array.from(guessedWordSet).filter((letter) =>
-    secretWordSet.has(letter),
-  ).length;
+  return [...guessedWordSet].filter((letter) => secretWordSet.has(letter))
+    .length;
 }
